a3aab28cae0ec243d3cf0479565c9789dfd55b31
Alex Theedom <alex.theedom@gmail.com>
2017-09-05 19:33:32 +0100

    Add example that collects to a JsonArray in a Java 8 stream

--- json-p/src/main/java/com/readlearncode/Java8Integration.java
+++ json-p/src/main/java/com/readlearncode/Java8Integration.java
@@ -1,7 +1,9 @@
 package com.readlearncode;
 
+import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonString;
+import javax.json.stream.JsonCollectors;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,11 +17,18 @@ public class Java8Integration extends JsonExample {
 
     private JsonObject jsonObject = loadJsonObject();
 
-    public List<String> filterJsonArray() {
+    public List<String> filterJsonArrayToList() {
         List<String> topics = jsonObject.getJsonArray("topics").stream()
                 .filter(jsonValue -> ((JsonString) jsonValue).getString().startsWith("C"))
                 .map(jsonValue -> ((JsonString) jsonValue).getString())
                 .collect(Collectors.toList());
+        return topics;
+    }
+
+    public JsonArray filterJsonArrayToJsonArray() {
+        JsonArray topics = jsonObject.getJsonArray("topics").stream()
+                .filter(jsonValue -> ((JsonString) jsonValue).getString().startsWith("C"))
+                .collect(JsonCollectors.toJsonArray());
         return topics;
     }
 
--- json-p/src/test/java/com/readlearncode/Java8IntegrationTest.java
+++ json-p/src/test/java/com/readlearncode/Java8IntegrationTest.java
@@ -2,6 +2,8 @@ package com.readlearncode;
 
 import org.junit.Test;
 
+import javax.json.Json;
+import javax.json.JsonArray;
 import java.util.List;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
@@ -15,12 +17,21 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 public class Java8IntegrationTest {
 
     @Test
-    public void givenJsonArray_shouldFilterAllTopicsStartingC() throws Exception {
+    public void givenJsonArray_shouldFilterAllTopicsStartingCToList() throws Exception {
         Java8Integration java8Integration = new Java8Integration();
-        List<String> topics = java8Integration.filterJsonArray();
+        List<String> topics = java8Integration.filterJsonArrayToList();
 
         assertThat(topics).contains("Cloud");
         assertThat(topics).contains("Cognitive");
+    }
+
+    @Test
+    public void givenJsonArray_shouldFilterAllTopicsStartingCToJsonArray() throws Exception {
+        Java8Integration java8Integration = new Java8Integration();
+        JsonArray topics = java8Integration.filterJsonArrayToJsonArray();
+
+        assertThat(topics.contains(Json.createValue("Cloud"))).isTrue();
+        assertThat(topics.contains(Json.createValue("Cognitive"))).isTrue();
     }
 
 }
