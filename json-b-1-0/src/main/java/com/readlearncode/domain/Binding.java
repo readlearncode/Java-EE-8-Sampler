800ce188763d4519c4a4713181f489134f92c19d
Alex Theedom <alex.theedom@gmail.com>
2017-09-09 18:57:41 +0100

    add Binding class

---
+++ json-b-1-0/src/main/java/com/readlearncode/domain/Binding.java
@@ -0,0 +1,12 @@
+package com.readlearncode.domain;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public enum Binding {
+
+    HARD_BACK, SOFT_BACK;
+}
