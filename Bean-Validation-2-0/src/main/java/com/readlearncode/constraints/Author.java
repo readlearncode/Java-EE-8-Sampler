e1bb9d7d5a7f69678a98c7c0ad9c78247abc44dd
Alex Theedom <alex.theedom@gmail.com>
2017-09-17 16:02:29 +0100

    add bean validation and security modules

--- Bean-Validation-2-0/src/main/java/com/readlearncode/constraints/Author.java
+++ Bean-Validation-2-0/src/main/java/com/readlearncode/constraints/Author.java
@@ -1,5 +1,6 @@
-package com.readlearncode.devWorks.overview;
+package com.readlearncode.constraints;
 
+import javax.validation.constraints.Email;
 import java.util.Objects;
 
 /**
@@ -12,13 +13,15 @@ public class Author {
 
     private String firstName;
     private String lastName;
+    private @Email String email;
 
     public Author() {
     }
 
-    public Author(String firstName, String lastName) {
+    public Author(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
     }
 
     public String getFirstName() {
@@ -37,18 +40,27 @@ public class Author {
         this.lastName = lastName;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Author author = (Author) o;
         return Objects.equals(firstName, author.firstName) &&
-                Objects.equals(lastName, author.lastName);
+                Objects.equals(lastName, author.lastName) &&
+                Objects.equals(email, author.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(firstName, lastName);
+        return Objects.hash(firstName, lastName, email);
     }
 
     @Override
@@ -56,6 +68,7 @@ public class Author {
         return "Author{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
                 '}';
     }
 }
---
+++ Bean-Validation-2-0/src/test/java/com/readlearncode/BookTest.java
@@ -0,0 +1,71 @@
+package com.readlearncode;
+
+import com.readlearncode.constraints.Author;
+import com.readlearncode.constraints.Binding;
+import com.readlearncode.constraints.Book;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.time.LocalDate;
+import java.time.Year;
+import java.util.*;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class BookTest {
+
+    private static Validator validator;
+
+    @BeforeClass
+    public static void setUpValidator() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void givenBeanWithConstraints_shouldValidate() {
+
+        Book book = new Book();
+        book.setId(UUID.randomUUID().toString());
+        book.setTitle(Optional.of("Fun with Java EE"));
+        book.setAuthor(new Author("Alex", "Theedom", "email@example.org.com.net"));
+        book.setPrice(10F);
+        book.setPages(200);
+        book.setDaysToPromotionEnd(-20);
+        book.setDaysToRelease(-10);
+        book.setReleased(Year.of(2017));
+        book.setNextVersionRelease(LocalDate.now());
+        book.setInPrint(true);
+        book.setBinding(Binding.SOFT_BACK);
+
+        HashMap<String, Book> map = new HashMap<>();
+        map.put("Non-Fiction", book);
+        book.setOtherBooksByAuthor(map);
+
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.add("Introduction");
+        book.setChapterTitles(list1);
+
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("French");
+        list2.add("English");
+        book.setLanguages(list2);
+
+        Set<ConstraintViolation<Book>> constraintViolations = validator.validate(book);
+
+        assertThat(constraintViolations.size()).isEqualTo(0);
+
+    }
+
+
+}
